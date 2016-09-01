ab1e253ddb575fdb499d445375a981d5426b5b04
root <root@nike.aristotle.net>
2016-09-01 02:07:16 -0500

    added gulp process scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 var gulp = require('gulp');
 var styleInject = require("gulp-style-inject");
+var scsslint = require('gulp-scsslint');
+var sass = require('gulp-sass');
 
-gulp.task('css', function() {
+gulp.task('sass', function () {
+  return gulp.src('components/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('components'));
+});
+ 
+gulp.task('lint', function() {
+  gulp.src('components/**/*.scss')
+    .pipe(scsslint({
+    	"config": "lint.yml"
+    }))
+    .pipe(scsslint.reporter());
+});
+
+gulp.task('build-components', function() {
   	return gulp.src("./components/**/*.html")
     	.pipe(styleInject())
     	.pipe(gulp.dest("./dist"));
